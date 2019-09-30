e6719e4f2bd71aaa2bc2402f59b7d8f74ce202f5
Mark Hoel <mdjhoel@gmail.com>
2019-09-30 14:58:55 -0400

    updates to make fancier
    thanks nasrudeen for helping debug

--- apitest.py
+++ apitest.py
@@ -1,9 +1,9 @@
 import requests
 from tkinter import *
-from webbrowser import open
+import webbrowser
 from os import path
 
-def writeHTML():
+def writeHTML(datajson,e1,e2):
     ofile = open("deleteme.html","w")
     ofile.write("<h1>" + datajson['city'] + "</h1>")
     ofile.write("<p>" + datajson['country'] + "</p>")
@@ -15,19 +15,28 @@ def writeHTML():
     ofile.write("osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';")
     ofile.write("osmAttrib='OpenStreetMap';")
     ofile.write("osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});")
-    ofile.write("map.setView(new L.LatLng(43,-79),13);")
+    ofile.write("map.setView(new L.LatLng(" + e1 + "," + e2 + "),13);")
     ofile.write("map.addLayer(osm);")
     ofile.write("</script>")
     ofile.write("")
     ofile.close()
 
 def getAPI():
-    myrequest  = requests.get("https://geo-info.co/43,-79")
+    myrequest  = requests.get("https://geo-info.co/" + e1.get() + "," + e2.get())
     datajson = myrequest.json()
-    b2 = Button(win,text="complete - view your file",command=open('file://' + path.realpath('deleteme.html')))
-    b2.grid(row=1,column=0)
+    writeHTML(datajson,e1.get(),e2.get())
+    b2 = Button(win,text="complete - view your file",command=webbrowser.open('file://' + path.realpath('deleteme.html')))
+    b2.grid(row=1,column=1)
     
 
 win = Tk()
+win.geometry('400x400')
+win.title('map a lat/long')
+
+e1 = Entry(win)
+e1.grid(row=0,column=0)
+e2 = Entry(win)
+e2.grid(row=0,column=1)
+
 b1 = Button(win,text="click me",command=getAPI)
-b1.grid(row=0,column=0)
+b1.grid(row=1,column=0)
